a5d0eda6f9701d2140f639bae4349db38cd4f2ea
s1azuan <shahrinazuan87@gmail.com>
2019-08-15 11:27:46 +0800

    Update day1.js

--- day1.js
+++ day1.js
@@ -1,3 +1,9 @@
+/*
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+For numbers divisible by 3, print "Fizz" instead of the number, 
+and for numbersdivisible by 5 (and not 3), print "Buzz" instead and"FizzBuzz" 
+for numbers that are divisible byboth 3 and 5
+*/
 
 var x=3;
 var y=5;
